20be203feae66ff03c4d0ee805f0afa723da1d44
Mikhail OKhapkin <mikhail.okhapkin@yandex.ru>
2022-11-18 15:48:51 +0300

    The fifth practice is ready

--- database/init.sql
+++ database/init.sql
@@ -1,35 +1,35 @@
-CREATE DATABASE IF NOT EXISTS main_database;
-
+CREATE DATABASE IF NOT EXISTS appDB default charset utf8;
 CREATE USER IF NOT EXISTS 'user'@'%' IDENTIFIED BY 'password';
-GRANT ALL ON main_database.* TO 'user'@'%';
+GRANT SELECT, UPDATE, INSERT ON appDB.* TO 'user'@'%';
 FLUSH PRIVILEGES;
 
-USE main_database;
-CREATE TABLE IF NOT EXISTS users (
-    id INT(10) NOT NULL AUTO_INCREMENT,
-    login VARCHAR(20) NOT NULL UNIQUE,
-    password VARCHAR(80) NOT NULL,
-    roles VARCHAR(10),
-    PRIMARY KEY (id)
+USE appDB;
+SET NAMES utf8;
+CREATE TABLE IF NOT EXISTS users
+(
+    id       INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    name     VARCHAR(100)    NOT NULL,
+    password VARCHAR(100)    NOT NULL,
+    role     VARCHAR(10)     NOT NULL
 );
-
-CREATE TABLE IF NOT EXISTS products (
-    id INT(10) NOT NULL AUTO_INCREMENT,
-    name_of_product VARCHAR(60) NOT NULL,
-    price DECIMAL(10,2) NOT NULL,
+CREATE TABLE IF NOT EXISTS products
+(
+    id          INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    name        VARCHAR(100)    NOT NULL,
+    volume      VARCHAR(100),
     description VARCHAR(100),
-    PRIMARY KEY (id)
+    price       INT             NOT NULL,
+    created     DATETIME
 );
 
-INSERT IGNORE INTO users
-SET id = 1,
-login = 'Mikhail',
-password = '$2y$10$zg8.a61TAaVe.IbijfV/9OcCK2mqWruVU9ZPDCt3LaV0kyfjIgj4K',
-roles = 'admin';
 
+INSERT INTO users (name, password, role)
+VALUES ('Mikhail', '123456', 'admin'),
+       ('test_user1', '123', 'user'),
+       ('test_user2', '456', 'user'),
+       ('test_user3', '789', 'user');
 
-
-INSERT INTO products (name_of_product, price)
+INSERT INTO products (name, price)
 VALUES
     ('Дверь входная', 17490),
     ('Светильник светодиодный', 749),
